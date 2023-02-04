ae924c7adcd426a244b1ca4d538d3cd8c0d4c40e
James Kane <james.kane59@outlook.com>
2023-02-04 09:21:19 +0000

    creating Skills component

---
+++ src/components/Skills.js
@@ -0,0 +1,33 @@
+import react, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+class Skills extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			skills: this.props.skills,
+		};
+	}
+
+	skillItems = this.state.skills.map((skill) => (
+		<SkillItem
+			id={skill.id}
+			onSkillChange={this.handleSkillsChange}
+			onSkillDelete={this.handleSkillsDelete}
+		/>
+	));
+
+	render() {
+		const { handleSkillsAdd } = this.props;
+
+		return (
+			<section>
+				<div>
+					<img src='' alt='skills-img' id='skills-img'></img>
+					<h2>Skills & Technologies</h2>
+				</div>
+				<div>{this.skillItems}</div>
+			</section>
+		);
+	}
+}
