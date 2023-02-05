665bf4b0c1857239ee02cd862980a3e4433dd390
James Kane <james.kane59@outlook.com>
2023-02-05 13:39:05 +0000

    creating preview contact aside component

---
+++ src/components/Preview/ContactPreview.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			website: this.props.website,
+			email: this.props.email,
+			location: this.props.location,
+			number: this.props.number,
+		};
+	}
+
+	render() {
+		return (
+			<aside>
+				<div>
+					<img src='' alt='website-img'></img>
+					<p>{this.website}</p>
+				</div>
+				<div>
+					<img src='' alt='email-img'></img>
+					<p>{this.email}</p>
+				</div>
+				<div>
+					<img src='' alt='location-img'></img>
+					<p>{this.location}</p>
+				</div>
+				<div>
+					<img src='' alt='phone-img'></img>
+					<p>{this.number}</p>
+				</div>
+			</aside>
+		);
+	}
+}
+
+export default Contact;
