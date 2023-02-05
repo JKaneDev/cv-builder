4d96bc3b880582d097bc81a9348bf135bddb1db7
James Kane <james.kane59@outlook.com>
2023-02-05 10:48:34 +0000

    refactoring Education component

--- src/components/Education.js
+++ src/components/Education.js
@@ -7,12 +7,11 @@ class Education extends Component {
 		this.state = {
 			education: this.props.education,
 		};
-		this.handleEducationChange = this.props.onEducationChange.bind(this);
-		this.handleEducationAdd = this.props.onEducationAdd.bind(this);
-		this.handleEducationDelete = this.props.onEducationDelete.bind(this);
 	}
 
 	render() {
+		const { onEducationChange, onEducationAdd, onEducationDelete, education } =
+			this.props;
 		return (
 			<div>
 				<h2>Educational Information</h2>
@@ -20,49 +19,41 @@ class Education extends Component {
 				<h3>University</h3>
 				<Input
 					type='text'
-					value={this.props.education[0].institution}
+					value={education[0].institution}
 					placeholder="E.g. Queen's University Belfast"
-					onChange={(event) =>
-						this.props.onEducationChange(id, 'institution', event)
-					}
+					onChange={(event) => onEducationChange(id, 'institution', event)}
 				/>
 				<Input
 					type='text'
-					value={this.props.education[0].degree}
+					value={education[0].degree}
 					placeholder='E.g. BEng Software Engineering'
-					onChange={(event) =>
-						this.props.onEducationChange(id, 'degree', event)
-					}
+					onChange={(event) => onEducationChange(id, 'degree', event)}
 				/>
 				<Input
 					type='text'
-					value={this.props.education[0].dates}
+					value={education[0].dates}
 					placeholder='E.g. 2015 - 2019'
-					onChange={(event) => this.props.onEducationChange(id, 'dates', event)}
+					onChange={(event) => onEducationChange(id, 'dates', event)}
 				/>
 				<img src='' alt='school svg' id='school-img' />
 				<h3>School</h3>
 				<Input
 					type='text'
-					value={this.props.education[1].institution}
+					value={education[1].institution}
 					placeholder='E.g. Rathmore Grammar School'
-					onChange={(event) =>
-						this.props.onEducationChange(id, 'institution', event)
-					}
+					onChange={(event) => onEducationChange(id, 'institution', event)}
 				/>
 				<Input
 					type='text'
-					value={this.props.education[1].degree}
+					value={education[1].degree}
 					placeholder='E.g. A-Levels: A A B'
-					onChange={(event) =>
-						this.props.onEducationChange(id, 'degree', event)
-					}
+					onChange={(event) => onEducationChange(id, 'degree', event)}
 				/>
 				<Input
 					type='text'
-					value={this.props.education[1].dates}
+					value={education[1].dates}
 					placeholder='E.g. 2008 - 2015'
-					onChange={(event) => this.props.onEducationChange(id, 'dates', event)}
+					onChange={(event) => onEducationChange(id, 'dates', event)}
 				/>
 			</div>
 		);
