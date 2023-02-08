ad38a35ff87b5923a18712ca42110b50acaa4ab6
James Kane <james.kane59@outlook.com>
2023-02-08 18:55:27 +0000

    styling skills and skill item components

--- src/components/SkillItem.js
+++ src/components/SkillItem.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Input from './UIElements/InputField';
 import Button from './UIElements/Button';
 import Delete from '../assets/delete.svg';
+import styled from 'styled-components';
 
 class SkillItem extends Component {
 	constructor(props) {
@@ -13,17 +14,57 @@ class SkillItem extends Component {
 		const { onSkillChange, onSkillDelete, id, skill } = this.props;
 
 		return (
-			<li>
+			<StyledSkillItem>
 				<Input
 					type='text'
 					value={skill}
 					placeholder='E.g. React'
 					onChange={(event) => onSkillChange(id, skill, event.target.value)}
 				/>
-				<Button img={Delete} onClick={() => onSkillDelete(id)} />
-			</li>
+				<Button
+					className='btn'
+					img={Delete}
+					onClick={() => onSkillDelete(id)}
+				/>
+			</StyledSkillItem>
 		);
 	}
 }
+
+const StyledSkillItem = styled.li`
+	display: flex;
+	align-items: center;
+
+	input {
+		width: 60%;
+	}
+
+	button {
+		width: 50px;
+		height: 30px;
+		border-radius: 8px;
+		border: none;
+		transition: 0.2s all ease-in-out;
+		margin-left: auto;
+		position: relative;
+		bottom: 0.5rem;
+	}
+
+	button:hover {
+		transform: translateY(-2.5%);
+		border: none;
+	}
+
+	button:hover img {
+		filter: ${({ theme }) => theme.filters.hoverFilterAlt};
+		transform: translateY(-2.5%);
+	}
+
+	button img {
+		height: 24px;
+		width: 24px;
+		transition: 0.1s all ease-in-out;
+	}
+`;
 
 export default SkillItem;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,6 +3,7 @@ import SkillItem from './SkillItem';
 import Button from './UIElements/Button';
 import SkillIcon from '../assets/skills.svg';
 import Add from '../assets/add.svg';
+import styled from 'styled-components';
 
 class Skills extends Component {
 	constructor(props) {
@@ -24,16 +25,65 @@ class Skills extends Component {
 		const { handleSkillsAdd } = this.props;
 
 		return (
-			<section>
+			<StyledSkills>
 				<div>
 					<img src={SkillIcon} alt='skills-img' id='skills-img'></img>
-					<h2>Skills & Technologies</h2>
+					<p className='title'>Skills & Technologies</p>
 				</div>
 				<ul>{this.skillItems}</ul>
 				<Button img={Add} onClick={handleSkillsAdd} />
-			</section>
+			</StyledSkills>
 		);
 	}
 }
+
+const StyledSkills = styled.section`
+	background-color: ${({ theme }) => theme.colors.subBackground};
+	padding: 2rem;
+	border-radius: 12px;
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+
+	div {
+		display: flex;
+		gap: 1rem;
+		align-items: center;
+	}
+
+	input {
+		background-color: ${({ theme }) => theme.colors.inputFields};
+		color: ${({ theme }) => theme.colors.fontColor};
+		height: 4ch;
+		padding: 0.35rem 1rem;
+		border-radius: 8px;
+		letter-spacing: 0.1rem;
+	}
+
+	.title {
+		font-size: 2.5ch;
+		letter-spacing: 0.3rem;
+		font-weight: bold;
+	}
+
+	img {
+		filter: ${({ theme }) => theme.filters.mainFilter};
+		transition: 0.1s all ease-in-out;
+	}
+
+	button {
+		margin-top: 1rem;
+		transition: 0.2s all ease-in-out;
+	}
+
+	button:hover {
+		border-bottom: 1px solid ${({ theme }) => theme.colors.elemBorderColor};
+		transform: translateY(-2.5%);
+	}
+
+	button:hover img {
+		filter: ${({ theme }) => theme.filters.hoverFilterAlt};
+	}
+`;
 
 export default Skills;
