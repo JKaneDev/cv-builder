d258e622c95a8b9b5f850fb93c881e3bae51b9bd
James Kane <james.kane59@outlook.com>
2023-02-16 18:47:34 +0000

    refactoring project from class based components to functional components

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,9 +1,18 @@
 const Theme = {
 	colors: {
 		mainBackground: '#525252',
-		subBackground: '#262626',
-		elemBorderColor: 'd97706',
+		subBackground: '#1a1a1d',
+		elemBorderColor: '#d97706',
 		fontColor: '#f5f5f5',
+		inputFields: '#313131',
+	},
+	filters: {
+		mainFilter:
+			'invert(95%) sepia(5%) saturate(239%) hue-rotate(201deg) brightness(116%) contrast(92%);',
+		hoverFilter:
+			'invert(100%) sepia(18%) saturate(471%) hue-rotate(207deg) brightness(112%) contrast(92%);',
+		hoverFilterAlt:
+			'invert(46%) sepia(66%) saturate(2313%) hue-rotate(10deg) brightness(97%) contrast(95%);',
 	},
 };
 
